5733a51e246ee8bb46b8dfeeb3ae481847cce52f
hamdihudhaidh <hamdisameer@hotmail.com>
2018-08-04 19:33:07 +0400

    adding the rest of the files that have been neglected in last commit

---
+++ PS4_Controller/Assets/Script/Player_Attack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attack : MonoBehaviour
+{
+    public Animator gunAnim;
+    public AudioClip attackAudio;
+
+    AudioSource gunAudio;
+    bool canAttack;
+
+	void Awake ()
+    {
+        gunAnim = GetComponent<Animator>();
+
+        canAttack = true;
+    }
+	
+
+	void Update ()
+    {
+        if (Input.GetButtonDown("R2") && canAttack == true)
+        {
+            print("pushed");
+            canAttack = false;
+            gunAnim.SetBool("Attack", true);
+            //add sound
+            //damage enemy with raycast
+        }
+	}
+
+    public void AllowAttack()
+    {
+        canAttack = true;
+        gunAnim.SetBool("Attack", false);
+    }
+}
